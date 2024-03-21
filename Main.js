5f804e1c10e664374bbdca6191a87b33660fa3f4
IAN.BALL <mrilovecutecats@gmail.com>
2024-03-21 09:35:55 -0500

    Made names better and moved assignment parsing function to new file

--- Main.js
+++ Main.js
@@ -1,18 +1,16 @@
-var ok;var nmh;(function(){ok=async function() {
-    document.body.innerHTML=null;
-    await fetch(`https://uview.instructure.com/api/v1/users/self/missing_submissions?include[]=course&filter[]=submittable&per_page={max}`, {
+let ok = async function () {
+    document.body.innerHTML = null;
+    await fetch(`https://uview.instructure.com/api/v1/users/self/missing_submissions?include[]=course&filter[]=submittable&per_page=${max}`, {
         "headers": {
             "accept": "application/json, text/javascript, application/json+canvas-string-ids, */*; q=0.01",
             "accept-language": "en-US,en;q=0.9,es;q=0.8",
             "cache-control": "no-cache",
             "pragma": "no-cache",
-            "sec-ch-ua": "\"Google Chrome\";v=\"89\", \"Chromium\";v=\"89\", \";Not A Brand\";v=\"99\"",
             "sec-ch-ua-mobile": "?0",
             "sec-fetch-dest": "empty",
             "sec-fetch-mode": "cors",
             "sec-fetch-site": "same-origin",
-            "x-csrf-token": "rr+YtvlKLjWe+ilDD1IqQvHLYQg1eptJPNV6jP13k2Hg6OnbrBocDMepeS9BYWEag4FZI1kV3AEL7DXNzS/bLA==",
-            "x-requested-with": "XMLHttpRequest"
+            "x-csrf-token": "rr+YtvlKLjWe+ilDD1IqQvHLYQg1eptJPNV6jP13k2Hg6OnbrBocDMepeS9BYWEag4FZI1kV3AEL7DXNzS/bLA=="
         },
         "referrer": "https://uview.instructure.com/",
         "referrerPolicy": "no-referrer-when-downgrade",
@@ -20,30 +18,16 @@ var ok;var nmh;(function(){ok=async function() {
         "method": "GET",
         "mode": "cors",
         "credentials": "include"
-    }).then(vc=>vc.json().then(vcd=>n=vcd));
-    var num=0;
-    n.forEach(function(entry) {
-        if((entry.locked_for_user===false)&&(entry.omit_from_final_grade===false)) {
-            num++
-            var nm=document.createElement("a");
-            nm.href=entry.html_url;
-            nm.innerText=entry.name;
-            nm.target="_blank";
-            var m=(document.createElement("div"));
-            var nn=document.createElement("div")
-            nn.appendChild(nm);
-            nn.appendChild(m);
-            tar=nn;
-            nn.style.borderColor="black";
-            nn.style.borderStyle="solid";
-            nn.style.borderBottomStyle="none";
-            document.body.appendChild(nn);
-        }
-    });
-    var mnm=document.createElement("strong");
-    mnm.innerText="you have ("+num+") missing assignments";
-    document.body.appendChild(mnm);
+    }).then(result => result.json().then(resBody => missingSubs = resBody));
+    //num is used to count the number of missing assignments
+    let num;
+
+    //numEl is a line of text that displays the number of missing assignments
+    let numEl = document.createElement("strong");
+    numEl.innerText = `you have "${num}" missing assignments`;
+    document.body.appendChild(numEl);
 };
-    nmh=setInterval(ok,((parseFloat(prompt("how long between updates in min","2")))*60000));
-    var tempmax=parseInt(prompt("max to list","1-100"))
-    let max=tempmax || 100})()
+let nmh = setInterval(ok, ((parseFloat(prompt("how long between updates in min", "2"))) * 60000));
+tempMax = parseInt(prompt("max to list", "1-100"))
+let max = tempMax || 100;
+let showBody=confirm("do you want the body of the assignment to be shown? (if you select cancel, only the assignment title will be shown)")
---
+++ parser.js
@@ -0,0 +1,29 @@
+function subParser(missingSubs) {
+    let count;
+    count = 0;
+    missingSubs.forEach(
+        function (missingSub) {
+            if ((missingSub.locked_for_user === false) && (missingSub.omit_from_final_grade === false)) {
+                count++
+                //assTitle displays the name of the missing assignment, and also the link to the assignment
+                let assTitle = document.createElement("a");
+                assTitle.href = missingSub.html_url;
+                assTitle.innerText = missingSub.name;
+                assTitle.target = "_blank";
+                //containerEl holds the title of the assignment and the body of the assignment
+                let containerEl = document.createElement("div")
+                containerEl.appendChild(assTitle);
+                if(showBody) {
+                    //assBody contains the "description" of the assignment, which is the body of the assignment
+                    let assBody = document.createElement("div");
+                    assBody.innerHTML = missingSub.description;
+                    containerEl.appendChild(assBody);
+                }
+                containerEl.style.borderColor = "black";
+                containerEl.style.borderStyle = "solid";
+                containerEl.style.borderBottomStyle = "none";
+                document.body.appendChild(containerEl);
+            }
+        });
+    return count;
+}
